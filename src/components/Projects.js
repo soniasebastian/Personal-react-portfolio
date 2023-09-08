87ee262389e139513a5b640d2f261172ebc7c647
soniasebastian <soniasebastian011@gmail.com>
2023-09-08 12:34:14 -0400

    added projects

---
+++ src/components/Projects.js
@@ -0,0 +1,46 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-4 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-blue-400">
+            Portfolio
+          </h1>
+          <h2 className="sm:text-2xl text-2xl text-white font-medium">
+            Each project is unique piece of development......
+          </h2>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={project.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-blue-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
