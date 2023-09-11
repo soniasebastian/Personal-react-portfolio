3d2a66f6abc8e71506db0a529f307b2cf3d305de
soniasebastian <soniasebastian011@gmail.com>
2023-09-11 16:54:30 -0400

    added changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-
 import Contact from "./components/Contact";
 
 export default function App() {
